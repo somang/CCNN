3480250017866a73376dc873305d02b20ddaa051
somang <sixjuwel@gmail.com>
2018-03-15 22:56:34 -0400

    finish the data gen
    and work on background

--- data_gen.py
+++ data_gen.py
@@ -3,12 +3,13 @@ import numpy as np
 from scipy.stats import skewnorm
 import matplotlib.pyplot as plt
 from random import randint
-
+import math
 
 gen_data = []
 #https://crtc.gc.ca/eng/archive/2012/2012-362.htm
 
 DATASIZE = 100
+portion = math.ceil(DATASIZE*0.1)
 
 # delay, wpm, similarity, number of errors
 r_delay = np.random.uniform(
@@ -16,94 +17,95 @@ r_delay = np.random.uniform(
 r_wpm = np.random.uniform(
   low=0.0, high=400.0, size=(DATASIZE,1))
 r_sentence_sim = np.random.uniform(
-  low=0.0, high=100.0, size=(DATASIZE,1)) # sentence cosine similarity
-r_spell_errors = np.random.randint(10, size=(DATASIZE,1)) # random number of spelling errors
-r_grammar_errors = np.random.randint(10, size=(DATASIZE,1)) # random number of grammar errors
+  low=0.0, high=100.0, size=(DATASIZE-portion,1)) # sentence cosine similarity
+r_spell_grammar_errors = np.random.randint(10, size=(DATASIZE,1)) # random number of spelling and grammar errors
 r_missing_words = np.random.randint(10, size=(DATASIZE,1)) # random number of missing words
 
-'''
+max_ss = []
+for i in range(portion):
+  max_ss.append([100.0])
+
+max_ss = np.asarray(max_ss)
+r_sentence_sim = np.concatenate((r_sentence_sim, max_ss))
+
 # paraphrasing factor:
-  #check sentence similarity (ss):
-  if ss == 100:
-    pf = 0
+pf_factors = []
+pf = 0
+for i in range(DATASIZE):
+  if i < 100:
+    missing_word_count = r_missing_words[i]
+    if missing_word_count > 0:
+      # when there are missing words, 
+      # then it means there has been paraphrasing
+      pf = 1
+    else:
+      pf = 0
   else:
-    if r_missing_words > 0:
-    # when there are missing words, 
-    # then it means there has been paraphrasing
-'''
+    pf = 0
+  pf_factors.append(pf)
+  print(r_sentence_sim[i], r_missing_words[i], pf)
+pf_factors = np.asarray(pf_factors)
+
+
+
+
+
 
 c = np.column_stack((r_delay, r_wpm))
 c = np.column_stack((c, r_sentence_sim))
-c = np.column_stack((c, r_spell_errors))
-c = np.column_stack((c, r_grammar_errors))
+c = np.column_stack((c, r_spell_grammar_errors))
 c = np.column_stack((c, r_missing_words))
+c = np.column_stack((c, pf_factors))
 
 # shuffle the order?
 np.random.shuffle(c)
-#print(c)
-# speed, delay, missing words, grammar errors, verbatim
-rating_list = [[],[],[]]#,[],[]]
+'''
+# [delay], [speed], [verbatim factor score], [grammar error score], [missing words score] 
+rating_list = [[],[],[],[],[]]
 for i in c:
-  #print(i)
-  speed_rating, delay_rating, misspell_rating, grammar_rating, verbatim_rating = 0,0,0,0,0
-
+  delay_score, speed_score, verbatim_score, spell_grammar_score = 0,0,0,0,0
   # calculate delay rating
   delay = i[0]
   if delay <= 100:
-    delay_rating = 10
+    delay_score = 10
   elif 100 < delay <= 500:
-    delay_rating = randint(8, 9)
+    delay_score = randint(8, 9)
   elif 500 < delay <= 2000:
-    delay_rating = randint(4, 7)
+    delay_score = randint(4, 7)
   elif 2000 < delay <= 4000:
-    delay_rating = randint(2, 4)
+    delay_score = randint(2, 4)
   else:
-    delay_rating = randint(0, 3)
+    delay_score = randint(0, 3)
 
   # calculate speed_rating
   wpm = i[1]
   if wpm <= 90:
-    speed_rating = randint(0,4) # when its too slow to read
+    speed_score = randint(0,4) # when its too slow to read
   elif 90 < wpm <= 100:
-    speed_rating = randint(5,8)
+    speed_score = randint(5,8)
   elif 100 < wpm <= 120:
-    speed_rating = randint(8,10)
-  elif 120 < wpm <= 220:
-    speed_rating = randint(5,8)
+    speed_score = randint(8,10)
+  elif 120 < wpm <= 140:
+    speed_score = randint(9,10)
+  elif 140 < wpm <= 220:
+    speed_score = randint(5,8)
   else:
-    speed_rating = randint(0,4)
+    speed_score = randint(0,4)
 
   # calculate grammar errors AND verbatim_ness
-  sentence_sim = i[2]
+  verbatim_score = i[2]
   if sentence_sim == 100:
-    grammar_rating = 10
-    verbatim_rating = 10
+    verbatim_score = 10
   elif 96 <= sentence_sim < 100:
-    verbatim_rating = randint(8,10)
-    grammar_rating = randint(0,10)
+    verbatim_score = randint(8,10)
   elif 90 <= sentence_sim < 96:
-    verbatim_rating = randint(4,7)
-    grammar_rating = randint(0,10)
+    verbatim_score = randint(4,7)
   else:
-    verbatim_rating = randint(0,3)
-    grammar_rating = randint(0,10)
+    verbatim_score = randint(0,3)
   
-  '''
-  # calculate misspell_rating
-  misspelled = i[3]
-  if misspelled == 0:
-    misspell_rating = 10
-  elif 0 < misspelled <= 3:
-    misspell_rating = randint(4,9)
-  else:
-    misspell_rating = randint(0,3)
-  '''
-
-  rating_list[0].append(speed_rating)
-  rating_list[1].append(delay_rating)
-  rating_list[2].append(verbatim_rating)
-  #rating_list[2].append(misspell_rating)
-  #rating_list[3].append(grammar_rating)
+  rating_list[0].append(delay_score)
+  rating_list[1].append(speed_score)
+  rating_list[2].append(verbatim_score)
 
 p = np.asarray(rating_list)
 
@@ -119,3 +121,4 @@ with open('gen_dt_100.csv', 'w') as mf:
   wr = csv.writer(mf)
   for i in c:
     wr.writerow(i)
+'''
