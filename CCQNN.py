a30dda859a1ed640968c1b7b326536cce717eb59
somang <sixjuwel@gmail.com>
2018-03-18 17:41:07 -0400

    minor

--- CCQNN.py
+++ CCQNN.py
@@ -12,69 +12,83 @@ import matplotlib.pyplot as plt
 import time
 mpl.rcParams['agg.path.chunksize'] = 10000
 
+DATAFILE = "gen_dt_100000.csv"
+MODEL_FILE = "emp_model.h5" 
+TRAINING = 1
 EPOCHS = 150
 VS_SWITCH = 0
 
-start = time.time()
-######################## data prep ###########################################
-dataset = np.loadtxt("gen_dt_100000.csv", delimiter=",")
-t_index = round(len(dataset)*0.7)
-# normalization and prep the data for train and validation
-sc = StandardScaler()
-X_data = sc.fit_transform(dataset[:,:6])
-X_data_emp, Y_data_emp = X_data[:,:4], dataset[:,6:10]
-X_data_ver, Y_data_ver = X_data[:,3:6], dataset[:,10:]
-# split input(X) and output(Y)
-emp_train_X = X_data_emp[:t_index,:]
-emp_train_Y = Y_data_emp[:t_index,:]
-emp_test_X = X_data_emp[t_index:,:]
-emp_test_Y = Y_data_emp[t_index:,:]
+def data_prep():
+  ######################## data prep ###########################################
+  dataset = np.loadtxt(DATAFILE, delimiter=",")
+  t_index = round(len(dataset)*0.7)
+  # normalization and prep the data for train and validation
+  sc = StandardScaler()
+  X_data = sc.fit_transform(dataset[:,:6])
+  X_data_emp, Y_data_emp = X_data[:,:4], dataset[:,6:10]
+  X_data_ver, Y_data_ver = X_data[:,3:6], dataset[:,10:]
+  # split input(X) and output(Y)
+  emp_tr_x = X_data_emp[:t_index,:]
+  emp_tr_y = Y_data_emp[:t_index,:]
+  emp_tst_x = X_data_emp[t_index:,:]
+  emp_tst_y = Y_data_emp[t_index:,:]
 
-ver_train_X = X_data_ver[:t_index,:]
-ver_train_Y = Y_data_ver[:t_index,:]
-ver_test_X = X_data_ver[t_index:,:]
-ver_test_Y = Y_data_ver[t_index:,:]
-######################## data prep ###########################################
+  ver_tr_x = X_data_ver[:t_index,:]
+  ver_tr_y = Y_data_ver[:t_index,:]
+  ver_tst_x = X_data_ver[t_index:,:]
+  ver_tst_y = Y_data_ver[t_index:,:]
+  ######################## data prep ###########################################
+  return emp_tr_x, emp_tr_y, emp_tst_x, emp_tst_y, ver_tr_x, ver_tr_y, ver_tst_x, ver_tst_y
 
 
-# create model using sequential
-emp_model = Sequential()
-emp_model.add(Dense(units=64, input_dim=4, activation='relu'))
-emp_model.add(Dense(units=32, activation='relu'))
-emp_model.add(Dense(units=4))
-
-# compile
-emp_model.compile(loss='mean_squared_error',
-              optimizer='adam', 
-              metrics=['accuracy']
-              )
-
-# fit the empirical value model
-emp_hist = emp_model.fit(emp_train_X, emp_train_Y, 
+def train_nn():
+  emp_tr_x, emp_tr_y, emp_tst_x, emp_tst_y, ver_tr_x, ver_tr_y, ver_tst_x, ver_tst_y = data_prep()
+  # create model using sequential
+  emp_model = Sequential()
+  emp_model.add(Dense(units=64, input_dim=4, activation='relu'))
+  emp_model.add(Dense(units=32, activation='relu'))
+  emp_model.add(Dense(units=4))
+  # compile
+  emp_model.compile(loss='mean_squared_error',
+                optimizer='adam', 
+                metrics=['accuracy'])
+  # fit the empirical value model
+  emp_hist = emp_model.fit(emp_tr_x, emp_tr_y, 
                 epochs=EPOCHS, batch_size=500,
-                verbose=1, validation_data=(emp_test_X, emp_test_Y)
+                verbose=1, validation_data=(emp_tst_x, emp_tst_y)
                 )
-#save the model
-emp_model.save('emp_model.h5') #creates a hdf5 file
+  #save the model
+  emp_model.save(MODEL_FILE) #creates a hdf5 file
+  # evaluate the empirical value model
+  loss_and_metrics = emp_model.evaluate(emp_tst_x, emp_tst_y, batch_size=50)
+  print("\n%s: %.2f%%" % (emp_model.metrics_names[1], loss_and_metrics[1]*100))
+  return emp_model, emp_hist
 
-# evaluate the empirical value model
-loss_and_metrics = emp_model.evaluate(emp_test_X, emp_test_Y, batch_size=50)
-print("\n%s: %.2f%%" % (emp_model.metrics_names[1], loss_and_metrics[1]*100))
+def draw_graphs(hist):
+  #val loss graph
+  loss = hist.history['loss']
+  val_loss = hist.history['val_loss']
+  plt.plot(loss)
+  plt.plot(val_loss)
+  plt.legend(['loss', 'val_loss'])
+  plt.show()
+  # draw the loss graph
+  acc = hist.history['acc']
+  val_acc = hist.history['val_acc']
+  plt.plot(acc)
+  plt.plot(val_acc)
+  plt.legend(['acc', 'val_acc'])
+  plt.show()
 
-#val loss graph
-loss = emp_hist.history['loss']
-val_loss = emp_hist.history['val_loss']
-plt.plot(loss)
-plt.plot(val_loss)
-plt.legend(['loss', 'val_loss'])
-plt.show()
-# draw the loss graph
-acc = emp_hist.history['acc']
-val_acc = emp_hist.history['val_acc']
-plt.plot(acc)
-plt.plot(val_acc)
-plt.legend(['acc', 'val_acc'])
-plt.show()
+if __name__ == '__main__':
+  if TRAINING:
+    model,hist = train_nn()
+    draw_graphs(hist)
+    
+  else:
+    model = load_nn()
+  predict(model)
+
 
 # graph the comparison between prediction vs real
 predictions = emp_model.predict(emp_test_X, batch_size=10)
