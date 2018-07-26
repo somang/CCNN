e3e8c567a010c20a82aa25fd93bfc76223972a58
somang <somang.nam@gmail.com>
2018-07-25 21:03:59 -0400

    minor edit

--- user_study_data/main.js
+++ user_study_data/main.js
@@ -1,1 +1,8 @@
-alert("hello")
+var tackname = 'Oceans_eng.vtt'
+
+
+var sourcename = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+
+
+
+document.getElementById("track").setAttribute("src") = trackname
