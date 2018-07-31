1b75a11e689b90c7f0c42b0da582c35802b3a514
somang <sixjuwel@gmail.com>
2018-07-31 19:19:43 -0400

    UPDATE

--- user_study_data/main.js
+++ user_study_data/main.js
@@ -1,7 +1,23 @@
-$( document ).ready(function() {
-    var sourcename = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+$(document).ready(function() {
 
+  // query string: ?foo=lorem&bar=&baz
+  var speed = getQueryVariable('s');
+  var delay = getQueryVariable('d'); 
+  var missing_words = getQueryVariable('m'); 
+  var paraphrased = getQueryVariable('p');
+  console.log(speed, delay, missing_words, paraphrased);
+  
 
-    var trackname = 'Oceans_eng.vtt';
-    $("track").attr("src", trackname);
 });
+
+
+function getQueryVariable(variable)
+{
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+          var pair = vars[i].split("=");
+          if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
